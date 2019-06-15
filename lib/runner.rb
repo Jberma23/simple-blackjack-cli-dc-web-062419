3728306b1c0da53a5cbf7ea51800495c07246b54
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-15 23:32:02 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,9 +1,10 @@
 require_relative "blackjack.rb"
+
 def greeting(welcome)
   puts "#{welcome}"
 end
 
-def initial_round(deal_card)
-puts "#{deal_card}"
-end
+# def initial_round(deal_card)
+# puts "#{deal_card}"
+# end
 # runner
