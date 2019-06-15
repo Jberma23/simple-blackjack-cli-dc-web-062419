61d4342759d89aac749448e853740506f5d65c2c
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-15 23:42:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,7 +6,7 @@ def deal_card
 rand(1..11)
 end
 
-def display_card_total(deal_card)
+def display_card_total(card_total)
   card_total = deal_card.chomp + deal_card.chomp
   puts "Your cards add up to #{card_total}"
 end
