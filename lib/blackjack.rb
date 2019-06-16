ee027126f9adf842b5f465bff197b3eb2108d912
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-16 00:25:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,11 +54,10 @@ end
 
 def runner
   welcome
-  initial_round
-  prompt_user
-  hit?
-  display_card_total
-  loop until card_total > 21
+  card_total = initial_round
+  until card_total > 21
+  card_total = hit?(card_total)
+  display_card_total(card_total)
 end
-return end_game
+  end_game(card_total)
 end
